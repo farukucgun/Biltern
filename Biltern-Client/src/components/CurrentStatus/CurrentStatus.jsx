be037cb9b848525637d6ef0bf01d2650fa0d3633
Faruk Uçgun <farukucgunxx@gmail.com>
2023-05-07 15:30:06 +0300

    Added client side authentication, alerts, sidebar

---
+++ Biltern-Client/src/components/CurrentStatus/CurrentStatus.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * @author Faruk Uçgun
+ * @date 07.05.2023
+ */
+
+const CurrentStatus = () => {
+    return (
+        <div>
+            <h1>Current Status</h1>
+        </div>
+    );
+}
+
+export default CurrentStatus;
