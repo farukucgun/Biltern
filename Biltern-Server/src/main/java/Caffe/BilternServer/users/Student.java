7748d6fc3dd8ba8610dfda8104fd10fae567ceb1
Faaiz Khan <faaizkhan4444@gmail.com>
2023-05-19 10:56:33 +0300

    added relations between student/TA/grader

--- Biltern-Server/src/main/java/Caffe/BilternServer/users/Student.java
+++ Biltern-Server/src/main/java/Caffe/BilternServer/users/Student.java
@@ -1,8 +1,8 @@
 package Caffe.BilternServer.users;
 
 import Caffe.BilternServer.auth.BilternUser;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 
 @Entity
 public class Student extends BilternUser {
@@ -10,11 +10,37 @@ public class Student extends BilternUser {
     @Column
     private String department;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "grader", referencedColumnName = "bilkentMail")
+    @JsonIgnore
+    private Grader grader;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "TA", referencedColumnName = "bilkentMail")
+    @JsonIgnore
+    private TeachingAssistant teachingAssistant;
+
     public String getDepartment() {
         return department;
     }
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    public Grader getGrader() {
+        return grader;
+    }
+
+    public void setGrader(Grader grader) {
+        this.grader = grader;
+    }
+
+    public TeachingAssistant getTeachingAssistant() {
+        return teachingAssistant;
+    }
+
+    public void setTeachingAssistant(TeachingAssistant teachingAssistant) {
+        this.teachingAssistant = teachingAssistant;
     }
 }
