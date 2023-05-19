c552dab24d062303b8aac1b42eaaaeb083893043
Faaiz Khan <faaizkhan4444@gmail.com>
2023-05-19 12:27:24 +0300

    added repository interfaces for all users, added service and controller classes for students

---
+++ Biltern-Server/src/main/java/Caffe/BilternServer/users/CoordinatorRepository.java
@@ -0,0 +1,9 @@
+package Caffe.BilternServer.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoordinatorRepository extends JpaRepository<Coordinator, Long> {
+
+}
---
+++ Biltern-Server/src/main/java/Caffe/BilternServer/users/GraderRepository.java
@@ -0,0 +1,9 @@
+package Caffe.BilternServer.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GraderRepository extends JpaRepository<Grader, Long> {
+
+}
