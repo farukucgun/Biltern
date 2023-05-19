d79a2e3f2a0e4522870acd7aa877983635e4c52e
Faaiz Khan <faaizkhan4444@gmail.com>
2023-05-19 10:56:33 +0300

    added relations between student/TA/grader

--- Biltern-Server/src/main/java/Caffe/BilternServer/users/Grader.java
+++ Biltern-Server/src/main/java/Caffe/BilternServer/users/Grader.java
@@ -1,10 +1,13 @@
 package Caffe.BilternServer.users;
 
 import Caffe.BilternServer.auth.BilternUser;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 
 import java.io.File;
+import java.util.List;
 
 @Entity
 public class Grader extends BilternUser {
@@ -14,6 +17,10 @@ public class Grader extends BilternUser {
 
     @Column
     private File signature;
+
+    @OneToMany(mappedBy = "grader")
+    @JsonIgnore
+    private List<Student> students;
 
     public Long getReportCount() {
         return reportCount;
@@ -29,5 +36,13 @@ public class Grader extends BilternUser {
 
     public void setSignature(File signature) {
         this.signature = signature;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
--- Biltern-Server/src/main/java/Caffe/BilternServer/users/TeachingAssistant.java
+++ Biltern-Server/src/main/java/Caffe/BilternServer/users/TeachingAssistant.java
@@ -1,8 +1,11 @@
 package Caffe.BilternServer.users;
 
 import Caffe.BilternServer.auth.BilternUser;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class TeachingAssistant extends BilternUser {
@@ -10,11 +13,23 @@ public class TeachingAssistant extends BilternUser {
     @Column
     private Long reportCount;
 
+    @OneToMany(mappedBy = "TA")
+    @JsonIgnore
+    private List<Student> students;
+
     public Long getReportCount() {
         return reportCount;
     }
 
     public void setReportCount(Long reportCount) {
         this.reportCount = reportCount;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
