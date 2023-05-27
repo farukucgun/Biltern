0872155dc175ee37af142fb80bc16fd1e255782e
Faruk Uçgun <farukucgunxx@gmail.com>
2023-05-27 11:08:40 +0300

    added custom hook, current status page change

--- Biltern-Client/src/components/Navbar/Navbar.jsx
+++ Biltern-Client/src/components/Navbar/Navbar.jsx
@@ -49,17 +49,8 @@ const Navbar = () => {
         </div>
       </div>
       {isAuthenticated ? <>
-
       {items.map((item, index) => <NavbarItem to={item.to} name={item.name} index={index} key={index}/>)}  
       <NavbarItem to="/login" name="Logout" onClick={logoutHandler}/>
-
-      {/* <NavbarItem to="/dashboard" name="Dashboard"/>
-      <NavbarItem to="/uploadedfiles" name="Uploaded Files"/>
-      <NavbarItem to="/currentstatus" name="Current Status"/>
-      <NavbarItem to="/settings" name="Settings"/>
-      <NavbarItem to="/semesterInitialization" name="Semester Initialization"/>
-      <NavbarItem to="/roleassignment" name="Role Assignment"/>
-      <NavbarItem to="/login" name="Logout" onClick={logoutHandler}/> */}
       </> : 
       <NavbarItem to="/login" name="Login"/> }
     </nav>
