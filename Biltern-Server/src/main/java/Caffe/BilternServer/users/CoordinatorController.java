85786a4a36ab7423aae6c36a1cf399c77b2043a5
Jank Old <cenkolcay22@gmail.com>
2023-05-28 22:26:08 +0300

    Adds cross origin annotations to controllers

--- Biltern-Server/src/main/java/Caffe/BilternServer/users/CoordinatorController.java
+++ Biltern-Server/src/main/java/Caffe/BilternServer/users/CoordinatorController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "${client.domain}")
 @RestController
 @RequestMapping("coordinator")
 public class CoordinatorController {
