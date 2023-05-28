100f6c03ff16a0c0e02cb54a83c776877b91bbe9
“ArshMarsh” <arshiabakhshayesh@yahoo.com>
2023-05-28 17:47:10 +0300

    assignment fixes, grading form fields fixes

--- Biltern-Server/src/main/java/Caffe/BilternServer/report/GradingForm/GradingFormService.java
+++ Biltern-Server/src/main/java/Caffe/BilternServer/report/GradingForm/GradingFormService.java
@@ -1,5 +1,6 @@
 package Caffe.BilternServer.report.GradingForm;
 
+import Caffe.BilternServer.report.Report;
 import com.itextpdf.text.DocumentException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.itextpdf.text.pdf.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Service
@@ -25,6 +28,7 @@ public class GradingFormService {
     public ByteArrayResource downloadGradingForm(Long reportId) {
         byte[] pdfTemplate = pdfTemplateRepository.findById(Long.valueOf(1)).get().getTemplate();
         GradingForm gradingForm =  gradingFormRepository.findByReportId(reportId).orElse(null);
+        Report report = gradingForm.getReport();
         Map<String, String> grades = gradingForm.getGrades();
         try {
             PdfReader reader = new PdfReader(pdfTemplate);
@@ -33,6 +37,17 @@ public class GradingFormService {
             PdfStamper stamper = new PdfStamper(reader, outputStream);
 
             AcroFields formFields = stamper.getAcroFields();
+
+            formFields.setField("name", report.getStudent().getUserName());
+            formFields.setField("name1", report.getStudent().getUserName());
+            formFields.setField("department", report.getStudent().getDepartment().toString());
+            formFields.setField("course", (
+                    report.getCourse().getCourseCode().equals("cs299") ? "choice1" : "choice2"));
+
+            LocalDate currentDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd");
+            String formattedDate = currentDate.format(formatter);
+            formFields.setField("date", formattedDate);
 
             for (Map.Entry<String, String> entry : grades.entrySet()) {
                 formFields.setField(entry.getKey().toLowerCase().trim(),entry.getValue());
