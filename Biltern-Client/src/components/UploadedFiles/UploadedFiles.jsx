5a408d9685f205763cb9a04551095ae51a659390
Enes Bektaş <enes.bektas@ug.bilkent.edu.tr>
2023-05-28 12:31:59 +0300

    added styles

--- Biltern-Client/src/components/UploadedFiles/UploadedFiles.jsx
+++ Biltern-Client/src/components/UploadedFiles/UploadedFiles.jsx
@@ -1,22 +1,54 @@
 import React from "react";
 import classes from "./UploadedFiles.module.css";
 import UploadedFilesItem from "./UploadedFilesItem";
+import axios from 'axios';
 
 /**
  * @author Enes Bektaş
  * @date 07.05.2023
  */
 
-const items = [
-    {to: "/displayfilepage", file: "CS319_AnalysisReport_Iter1_Caffe (2).pdf"},
-    {to: "/displayfilepage", file: "blablabla..pdf"},
-    {to: "/displayfilepage", file: "...pdf"},
-    {to: "/displayfilepage", file: "...pdf"}
-]
+
 const UploadedFiles = () => {
+
+    const [url, setUrl] = React.useState("");
+
+    React.useEffect(() => {
+        const fetchReport = async () => {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": localStorage.getItem("token") || ""
+                }
+            };
+
+            await axios.get("http://localhost:8080/report/reportContent/1", {config, responseType: 'arraybuffer'})
+                .then(res => {
+                    const blob = new Blob([res.data], {type: 'application/pdf'});
+                    setUrl(URL.createObjectURL(blob));
+                })
+                .catch(err => {
+                    // dispatch(setTimedAlert({msg: "Error while fetching notifications", alertType: "error", timeout: 4000}));
+                    console.log(err);
+                });
+        };
+
+        fetchReport();
+    }, []);
+
+    console.log(url)
+
+    const items = [
+        {to: "/displayfilepage", fileURL: url, fileName: "CS319_AnalysisReport_Iter1_Caffe (2).pdf"},
+        {to: "/displayfilepage", fileURL: url, fileName: "...pdf"},
+        {to: "/displayfilepage", fileURL: url, fileName: "...pdf"},
+        {to: "/displayfilepage", fileURL: url, fileName: "...pdf"}
+    ]
+
+
     return (
         <div className={classes.uploaded_files_container}>
-            {items.map((item, index) => <UploadedFilesItem to={item.to} pdfFile={item.file} index={index} key={index} />)}
+            {items.map((item, index) => <UploadedFilesItem to={item.to} fileURL={item.fileURL} fileName={item.fileName} index={index} key={index} />)}
         </div>
     );
 }
