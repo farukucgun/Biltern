42bc59e798984f2325b08a62ff4a7bde06a33e53
Jank Old <cenkolcay22@gmail.com>
2023-05-29 02:26:57 +0300

    Changes for cascading problem in Course Entity

--- Biltern-Server/src/main/java/Caffe/BilternServer/course/CourseService.java
+++ Biltern-Server/src/main/java/Caffe/BilternServer/course/CourseService.java
@@ -34,4 +34,11 @@ public class CourseService {
         return courseRepository.findByCourseCode(courseCode).orElseThrow(
                 () -> new EntityNotFoundException());
     }
+
+
+    public void deleteCourse(Long id){
+        Course course = courseRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException());
+        courseRepository.delete(course);
+    }
 }
