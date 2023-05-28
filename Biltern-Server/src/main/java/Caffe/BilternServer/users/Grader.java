14d75a9cf8aaf31c957a553a2bf5d437c58b94be
“ArshMarsh” <arshiabakhshayesh@yahoo.com>
2023-05-28 22:17:00 +0300

    retribution for messing up faaiz's repository

--- Biltern-Server/src/main/java/Caffe/BilternServer/users/Grader.java
+++ Biltern-Server/src/main/java/Caffe/BilternServer/users/Grader.java
@@ -3,13 +3,8 @@ package Caffe.BilternServer.users;
 import Caffe.BilternServer.report.Report;
 import Caffe.BilternServer.auth.BilternUser;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Data;
-
-import java.io.File;
 import java.util.List;
 
 @Data
@@ -19,7 +14,8 @@ public class Grader extends BilternUser {
     @Column
     private Long reportCount;
 
-    @Column
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
     private byte[] signature;
 
     @OneToMany(mappedBy = "grader", fetch = FetchType.LAZY)
